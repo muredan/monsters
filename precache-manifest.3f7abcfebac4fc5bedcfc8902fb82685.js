45e57d6330dff72454a2b46e25a77d89f7dbe5df
mured <muredan@yahoo.com>
2020-03-26 00:28:27 +0200

    Updates

---
+++ precache-manifest.3f7abcfebac4fc5bedcfc8902fb82685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8326eb6e0b5ab15b43607f761130fd7c",
+    "url": "/muredan/monsters.git/index.html"
+  },
+  {
+    "revision": "556aeb836cbe76437fd5",
+    "url": "/muredan/monsters.git/static/css/main.1777ea13.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/muredan/monsters.git/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/muredan/monsters.git/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "556aeb836cbe76437fd5",
+    "url": "/muredan/monsters.git/static/js/main.5171f4e4.chunk.js"
+  },
+  {
+    "revision": "f5ede9d869088f3af188",
+    "url": "/muredan/monsters.git/static/js/runtime-main.d8245c3f.js"
+  }
+]);
